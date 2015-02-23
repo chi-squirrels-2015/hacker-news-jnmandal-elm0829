2c8dbb7ec9e1fc1577fac29d1fca01c08ce28ae2
Edward Mitchell & John N. Mandalakas <pair+elm0829.jnmandal@devbootcamp.com>
2015-02-23 13:22:32 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+10.times do
+  User.create!(name: Faker::Internet.user_name, password: "password", password_confirmation: "password", about: Faker::Lorem.paragraph)
+end
+
+users = User.all
+
+20.times do
+  Post.create!(title: Faker::Lorem.sentence, link: Faker::Internet.url, user: users.sample)
+end
+
+posts = Post.all
+
+100.times do
+  Comment.create!(body: Faker::Lorem.paragraph, user: users.sample, post: posts.sample)
+end
+
+comments = Comment.all
+
+posts.each do |post|
+  rand(100).times {Vote.create!(user: users.sample, post: post)}
+end
+
+comments.each do |comment|
+  rand(20).times {Vote.create!(user: users.sample, comment: comment)}
+end
+
+User.create!(name: "jnmandal", password: "password", password_confirmation: "password")
+User.create!(name: "elm0829", password: "password", password_confirmation: "password")
